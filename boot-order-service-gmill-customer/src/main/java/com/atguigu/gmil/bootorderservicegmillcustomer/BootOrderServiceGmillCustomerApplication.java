618e2b5f986f6b2e809274d16f886c19fa4f358d
wangxuan <wangxuan@finogeeks.com>
2018-10-05 14:26:58 +0800

    init

---
+++ boot-order-service-gmill-customer/src/main/java/com/atguigu/gmil/bootorderservicegmillcustomer/BootOrderServiceGmillCustomerApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmil.bootorderservicegmillcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootOrderServiceGmillCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootOrderServiceGmillCustomerApplication.class, args);
+	}
+}
